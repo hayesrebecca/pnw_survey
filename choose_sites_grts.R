29954b773d72828781d7f939f36776d2adf944b6
Lauren Ponisio <lponisio@gmail.com>
2022-04-06 14:21:32 -0700

    tided file paths, and moved function to subsite fires/sites and calculate new sites into src

---
+++ choose_sites_grts.R
@@ -0,0 +1,83 @@
+rm(list=ls())
+set.seed(9)
+library(terra)
+library(spsurvey)
+library(tidyverse)
+library(sf)
+
+## rebecca
+## setwd("C:/Users/rah10/Dropbox (University of Oregon)/")
+
+## Lauren mac pro
+setwd("/Volumes/bombus/Dropbox (University of Oregon)/")
+
+## Jesse
+
+setwd("pnw_survey_saved/")
+
+grts.save.dir <- "spatial_data"
+
+source("../pnw_survey/src/grts_design_2022.R")
+source("../pnw_survey/src/site_select_grts.R")
+
+## read in shape file of the 2020 and other fires in OR and CA
+fire_polygons <- sf::read_sf("spatial_data/SeverityMerge_220308")
+
+## read in the legacy sites from 2021 (already converted into points)
+sites2021 <- sf::read_sf("spatial_data/NCASI_GIS_2022/2021stands_points.shp")
+
+## change crs to match Firenames
+sites2021 <- st_transform(sites2021, st_crs(fire_polygons))
+
+## get rid of random bad sites with no x y
+sites2021 <- sites2021[!is.na(sites2021$Watershed),]
+
+## subset sites to the watersheds with in each fire
+holiday_watersheds <- c("W56Johnson", "W57Ritchie", "W55Indian")
+dixie_watersheds <- c("W58MoodyMd", "W59Sheepca", "W62Poplar",  "W60Rock")
+claremont_watersheds <- c("W61Willow")
+beachie_watersheds <- c("W50Pine", "W51Molalla")
+## there are no sites from 2021 in the riverside fire
+
+
+claremont_grts <- site_select_grts(all_legacy_sites = sites2021,
+                                   watersheds= claremont_watersheds,
+                                   all_fire_polygons = fire_polygons,
+                                   fire.name = "Claremont",
+                                   design_vector = claremont_design,
+                                   save.dir  = grts.save.dir)
+
+
+holiday_grts <- site_select_grts(all_legacy_sites = sites2021,
+                                 watersheds= holiday_watersheds,
+                                 all_fire_polygons = fire_polygons,
+                                 fire.name = "Holiday",
+                                 design_vector = holiday_design,
+                                 save.dir  = grts.save.dir)
+
+
+dixie_grts <- site_select_grts(all_legacy_sites = sites2021,
+                               watersheds= holiday_watersheds,
+                               all_fire_polygons = fire_polygons,
+                               fire.name = "Dixie",
+                               design_vector = dixie_design,
+                               save.dir  = grts.save.dir)
+
+beachie_grts <- site_select_grts(all_legacy_sites = sites2021,
+                                 watersheds= beachie_watersheds,
+                                 all_fire_polygons = fire_polygons,
+                                 fire.name = "Beachie",
+                                 design_vector = beachie_design,
+                                 save.dir  = grts.save.dir)
+
+## not passing in legacy sites because there were none samples in 2021
+riverside_grts <- site_select_grts(all_fire_polygons = fire_polygons,
+                                   fire.name = "Riverside",
+                                   design_vector = riverside_design,
+                                   save.dir  = grts.save.dir)
+
+
+
+
+
+
---
+++ src/site_select_grts.R
@@ -0,0 +1,40 @@
+
+site_select_grts <- function(all_legacy_sites=NULL,  ## all the legancy sites from 2021
+                             watersheds=NULL, ## a vector of the watershes in the fire of interest
+                             all_fire_polygons,  ## the giant merged fire polygons
+                             fire.name, ## fire of interest as a character
+                             design_vector, ##design input for grts vector
+                             save.dir ## director to save
+                             ){
+                                        #filter to just include specific fire polygons
+    subset_fire <- all_fire_polygons[all_fire_polygons$Fire_Name == fire.name, ]
+
+    if(!is.null(all_legacy_sites)){
+        print(fire.name)
+        ## subset to the correct watershes
+        subset_2021_sites <-
+            all_legacy_sites[all_legacy_sites$Watershed %in%
+                             watersheds,]
+        ## run grts with legacy sites
+        strat_eqprob <- grts(subset_fire,
+                             n_base = design_vector,
+                             stratum_var = "Strata",
+                             legacy_sites=subset_2021_sites,
+                             legacy_stratum_var = 'Watershed',
+                             mindis=1000)
+    } else{
+        ## run grts without legacy sites
+        strat_eqprob <- grts(subset_fire,
+                             n_base = design_vector,
+                             stratum_var = "Strata",
+                             mindis=1000)
+    }
+
+    ## export the sites selected
+    st_write(strat_eqprob$sites_base,
+             dsn= file.path(save.dir, sprintf("grts_sites/%s.shp", fire.name))
+             )
+
+    return(strat_eqprob)
+
+}
