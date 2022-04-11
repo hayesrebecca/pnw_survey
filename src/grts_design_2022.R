6ffb8ff7a9fa83bc2d9da4d9d0bbf7a239632284
lponisio <lponisio@gmail.com>
2022-04-11 11:43:03 -0700

    changed design file to have more sites as buffer

--- src/grts_design_2022.R
+++ src/grts_design_2022.R
@@ -1,20 +1,20 @@
 
 beachie_design <- c( #choose the number of points in each firesev category
-    'highBLM' = 2,
+    'highBLM' = 4,
     'medBLM' = 2,
-    'lowBLM' = 2,
-    'highE' = 2,
+    ## 'lowBLM' = 2,
+    'highE' = 4,
     'medE' = 2,
-    'lowE' = 2,
-    'highM' = 2,
+    ## 'lowE' = 2,
+    'highM' = 4,
     'medM' = 2,
-    'lowM' = 2,
-    'highSTATE' = 2,
+    ## 'lowM' = 2,
+    'highSTATE' = 4,
     'medSTATE' = 2,
-    'lowSTATE' = 2,
-    'highUSFS' = 2,
-    'medUSFS' = 2,
-    'lowUSFS' = 2
+    ## 'lowSTATE' = 2,
+    'highUSFS' = 4,
+    'medUSFS' = 2
+    ## 'lowUSFS' = 2
 )
 
 ## beachie_caty <- list( #choose the number of points for each owner, must equal number in design
@@ -40,8 +40,8 @@ beachie_design <- c( #choose the number of points in each firesev category
 
 claremont_design <- c( #choose the number of points in each firesev category
   'highC' = 10,
-  'medC' = 10,
-  'lowC' = 10
+  'medC' = 10
+  ## 'lowC' = 10
 )
 
 ## claremont_caty <- list( #choose the number of points for each owner, must equal number in design (in legacy some sites are NA for owner)
@@ -55,21 +55,21 @@ claremont_design <- c( #choose the number of points in each firesev category
 
 
 riverside_design <- c( #choose the number of points in each firesev category
-    'highBLM' = 2,
+    'highBLM' = 4,
     'medBLM' = 2,
-    'lowBLM' = 2,
-    'highE' = 2,
+    ## 'lowBLM' = 2,
+    'highE' = 4,
     'medE' = 2,
-    'lowE' = 2,
-    'highM' = 2,
+    ## 'lowE' = 2,
+    'highM' = 4,
     'medM' = 2,
-    'lowM' = 2,
+    ## 'lowM' = 2,
     ## 'highSTATE' = 2,
     'medSTATE' = 2,
-    'lowSTATE' = 2,
-    'highUSFS' = 2,
-    'medUSFS' = 2,
-    'lowUSFS' = 2
+    ## 'lowSTATE' = 2,
+    'highUSFS' = 4,
+    'medUSFS' = 2
+    ## 'lowUSFS' = 2
 )
 
 ## riverside_caty <- list( #choose the number of points for each owner, must equal number in design
@@ -91,24 +91,24 @@ riverside_design <- c( #choose the number of points in each firesev category
 ## )
 
 holiday_design <- c( #choose the number of points in each firesev category
-    'highBLM' = 2,
+    'highBLM' = 4,
     'medBLM' = 2,
-    'lowBLM' = 2,
-    'highT' = 2,
+    ## 'lowBLM' = 2,
+    'highT' = 4,
     'medT' = 2,
-    'lowT' = 2,
+    ## 'lowT' = 2,
     'highM' = 4,
     'medM' = 2,
-    'lowM' = 2,
-    'highSTATE' = 2,
+    ## 'lowM' = 2,
+    'highSTATE' = 4,
     'medSTATE' = 2,
-    'lowSTATE' = 2,
-    'highUSFS' = 2,
+    ## 'lowSTATE' = 2,
+    'highUSFS' = 4,
     'medUSFS' = 2,
-    'lowUSFS' = 2,
-    'highU' = 2,
-    'medU' = 2,
-    'lowU' = 2
+    ## 'lowUSFS' = 2,
+    'highU' = 4,
+    'medU' = 2
+    ## 'lowU' = 2
 )
 
 ## holiday_caty <- list( #choose the number of points for each owner, must equal number in design
@@ -134,12 +134,12 @@ holiday_design <- c( #choose the number of points in each firesev category
 
 
 dixie_design <- c( #choose the number of points in each firesev category
-    'highC' = 5,
+    'highC' = 10,
     'medC' = 5,
-    'lowC' = 5,
-    'highQ' = 5,
-    'medQ' = 5,
-    'lowQ' = 5
+    ## 'lowC' = 5,
+    'highQ' = 10,
+    'medQ' = 5
+    ## 'lowQ' = 5
     )
 
 ## dixie_caty <- list( #choose the number of points for each owner, must equal number in design
